a4d1113b8b56c4d8aabf6b6a308e52dff5ea241b
maarijwaseem <maarijwaseem7@gmail.com>
2024-05-07 01:34:51 +0500

    New Concept

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,13 +1,20 @@
+import { notFound } from "next/navigation";
 export default function ReviewDetail({
     params,
   }: {
     params: {
+      productId: string;
       reviewId: string;
+
     };
   }) {
+    if( parseInt(params.reviewId) > 1000){
+      notFound();
+    }
+
     return (
       <>
-        <h1>Reviews Details {params.reviewId}</h1>
+        <h1>Reviews {params.reviewId} for product {params.productId}</h1>
       </>
     );
   }
