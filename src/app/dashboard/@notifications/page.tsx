a20262e7fc49ac3800415f6bfb9df08f63915d3f
maarijwaseem <maarijwaseem7@gmail.com>
2024-05-09 23:09:35 +0500

    All routing Completed

---
+++ src/app/dashboard/@notifications/page.tsx
@@ -0,0 +1,9 @@
+import Card from "@/components/card";
+
+export default function Notifications(){
+    return (
+        <div>
+            <Card>Notification</Card>
+        </div>
+    )
+}
---
+++ src/app/dashboard/@revenue/page.tsx
@@ -0,0 +1,9 @@
+import Card from "@/components/card";
+
+export default function RevenueMetrics(){
+    return (
+        <div>
+            <Card>Revenue</Card>
+        </div>
+    )
+}
